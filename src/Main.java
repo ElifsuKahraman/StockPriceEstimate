c8be99c9991f5f8078e01f6fb09ae96734d67514
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-20 18:04:06 +0300

    print file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.io.File;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file=new File("C:/Users/kelif/Desktop/stock_price.csv");
+        FileRead fileRead=new FileRead(file);
+        fileRead.readAllData();
+
     }
+
 }
