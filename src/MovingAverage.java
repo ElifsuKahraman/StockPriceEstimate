675f3ced4db7fc39ae034a004d57b6a01c78b0df
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-22 17:43:14 +0300

    update moving average

--- src/MovingAverage.java
+++ src/MovingAverage.java
@@ -4,9 +4,10 @@ public class MovingAverage {
    FileRead fileRead;
    Double sma;
 
-    public MovingAverage(FileRead fileRead){
+   public MovingAverage(FileRead fileRead){
         this.fileRead=fileRead;
     }
+
     public double CalculateSma() {
         double total = 0;
         for (Double p : fileRead.priceArray) {
@@ -18,35 +19,32 @@ public class MovingAverage {
 
     public double CalculateWma(){
         ArrayList<Double> weight=new ArrayList<>();
+        double weightedMultiplication =0;
+        double weightedTotal =0;
         for(double i=1;i<=10;i++){
             weight.add(i);
         }
-        double agirliklicarpim=0;
-        double agirliklitoplam=0;
         for(int i=0;i<fileRead.priceArray.size();i++){
             double price =fileRead.priceArray.get(i);
             double weight1 =weight.get(i);
-            agirliklicarpim+= price * weight1;
-            agirliklitoplam+= weight1;
+            weightedMultiplication += price * weight1;
+            weightedTotal += weight1;
         }
-        double wma=agirliklicarpim/agirliklitoplam;
+        double wma= weightedMultiplication / weightedTotal;
         return wma;
     }
 
     public double CalculateEma(){
         double period=fileRead.priceArray.size();;
-        double carpan=2.0/(period+1);;
+        double multiplier =2.0/(period+1);;
 
+        double firstEmaValue =CalculateSma();
+        double ema= firstEmaValue;
 
-        double ilkemadegeri=CalculateSma();
-        double ema=ilkemadegeri;
-
-        for(double i=period; i<fileRead.priceArray.size();i++){
-            double kapanisfiyat= fileRead.priceArray.get((int) i);
-            ema=(kapanisfiyat*carpan)+(ema*(1-carpan));
-        }
-        return ema;
-    }
-
+//        for(double i=0; i<fileRead.priceArray.size();i++) {
+            double closingPrice = fileRead.priceArray.get(fileRead.priceArray.size()-1);
+            ema = ((closingPrice - ema) * multiplier) + ema;
+            return ema;
+   }
 }
 
