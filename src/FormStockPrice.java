a60cf13c95f479cf9dd56f42660b7e88beaf1bd8
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-22 17:46:39 +0300

    update formstockprice button

--- src/FormStockPrice.java
+++ src/FormStockPrice.java
@@ -1,23 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.dnd.DropTarget;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-public class FormStockPrice extends JFrame {
+public class FormStockPrice extends JFrame implements ActionListener {
     private JButton selectFileButton;
     private JButton SmaButton;
     private JPanel panel1;
     private JButton EmaButton;
     private JButton WmaButton;
-    private JTextArea textArea1;
-    private File file;
+    private JTextArea textArea1=new JTextArea(3, 3);
     private boolean fileSelected = false;
     private File selectedFile;
+    private FileRead fileRead;
 
     public FormStockPrice() {
         add(panel1);
-        setSize(600, 600);
+        panel1.setSize(800, 400);
         setTitle("Tahmin");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -30,50 +31,14 @@ public class FormStockPrice extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
-        SmaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (fileSelected) {
-                    FileRead fileRead = new FileRead(file);
-                    fileRead.readAllData();
-                    MovingAverage movingAverage = new MovingAverage(fileRead);
-                    double guessSma = movingAverage.CalculateSma();
-                    JOptionPane.showMessageDialog(null, "Basit Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessSma);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Lütfen bir dosya seçin.");
-                }
-            }
-        });
-        WmaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (fileSelected) {
-                    FileRead fileRead = new FileRead(file);
-                    fileRead.readAllData();
-                    MovingAverage movingAverage = new MovingAverage(fileRead);
-                    double guessWma = movingAverage.CalculateWma();
-                    JOptionPane.showMessageDialog(null, "Ağırlıklı Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessWma);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Lütfen bir dosya seçin.");
-                }
+        DragAndDrop drag = new DragAndDrop(textArea1, this);
+        new DropTarget(textArea1,drag);
 
-            }
-        });
-        EmaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (fileSelected) {
-                    FileRead fileRead = new FileRead(file);
-                    fileRead.readAllData();
-                    MovingAverage movingAverage = new MovingAverage(fileRead);
-                    double guessEma = movingAverage.CalculateEma();
-                    JOptionPane.showMessageDialog(null, "Üssel Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessEma);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Lütfen bir dosya seçin.");
-                }
+        SmaButton.addActionListener(this);
 
-            }
-        });
+        WmaButton.addActionListener(this);
+
+        EmaButton.addActionListener(this);
 
         selectFileButton.addActionListener(new ActionListener() {
             @Override
@@ -87,18 +52,50 @@ public class FormStockPrice extends JFrame {
                         JOptionPane.showMessageDialog(null, "Dosya başarıyla eklendi: " + selectedFile.getName());
                         fileSelected = true;
                         selectFileButton.setEnabled(false);
-                        file=selectedFile;
                     }
                 } else {
                     JOptionPane.showMessageDialog(null, "Zaten bir dosya seçildi. Sürükleyip bırakma yöntemi kullanılamaz.");
                 }
             }
         });
-
-
     }
-    public void dosya(){
+    public void setFileRead(FileRead fileRead) {
+        this.fileRead=fileRead;
+    }
+    public void setFile(File file) {
+        this.selectedFile = file;
+        this.fileSelected = true;
+        this.fileRead = new FileRead(file);
+    }
+    public void disableSelectFileButton() {
+        selectFileButton.setEnabled(false);
+    }
+    public boolean isFileSelected() {
+        return fileSelected;
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (selectedFile!=null) {
+            FileRead fileRead = new FileRead(selectedFile);
+            fileRead.readAllData();
+            fileRead.checkData();
+            MovingAverage movingAverage = new MovingAverage(fileRead);
+            if(e.getActionCommand().equals("Üssel Hareketli Ortalama")){
+                double guessEma = movingAverage.CalculateEma();
+                JOptionPane.showMessageDialog(null, "Üssel Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessEma);
+            }
+            else if(e.getActionCommand().equals("Ağırlıklı Hareketli Ortalama")){
+                double guessWma = movingAverage.CalculateWma();
+                JOptionPane.showMessageDialog(null, "Ağırlıklı Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessWma);
+            }
+            else if(e.getActionCommand().equals("Basit Hareketli Ortalama")){
+                double guessSma = movingAverage.CalculateSma();
+                JOptionPane.showMessageDialog(null, "Basit Hareketli Ortalamaya Göre Tahmin Fiyatı: " + guessSma);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Lütfen bir dosya seçin.");
+        }
     }
 }
 
